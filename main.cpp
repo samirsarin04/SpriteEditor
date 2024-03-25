01110a9dcb05612bf397f8256d21204a4d98e349
eheathco <125215367+eheathco@users.noreply.github.com>
2024-03-25 10:55:53 -0600

    Project Skeleton

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "view.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Model m;
+    View w(m);
+    w.show();
+    return a.exec();
+}
---
+++ view.cpp
@@ -0,0 +1,15 @@
+#include "view.h"
+#include "ui_view.h"\
+#include "model.h"
+
+View::View(Model& model, QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::View)
+{
+    ui->setupUi(this);
+}
+
+View::~View()
+{
+    delete ui;
+}
