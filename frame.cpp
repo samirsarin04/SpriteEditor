b8b0b6259984824d74fe2cdcc6b522da1c653e37
samirsarin04 <mrgrooviness@gmail.com>
2024-04-01 21:29:47 -0600

    updated comments

--- frame.cpp
+++ frame.cpp
@@ -3,11 +3,13 @@
 
 int Frame::globalID = 0;
 Frame::Frame(int pixelDimension) : pixelDimension(pixelDimension), frameImage(pixelDimension, pixelDimension, QImage::Format_RGBA64) {
-    //Default initializes everything
+    //sets the ID equal to
     ID = globalID;
     ++globalID;
     pixelSize = (640 / pixelDimension);
     canvasSize = pixelSize * pixelDimension;
+
+    //pixels should be set to transparent when creating a frame
     for(int i = 0; i < (pixelDimension * pixelDimension); i++){
         pixels.push_back(QColor(0, 0, 0, 0));
     }
@@ -24,6 +26,7 @@ QVector<QColor> Frame::getPixels() {
 }
 
 QVector<QColor> Frame::addNewPixel(int x, int y, QColor color){
+    //modify the pixels and generate the image
     modifyPixel(pixels, x, y, color);
     generateImage();
     return pixels;
@@ -45,6 +48,7 @@ QImage Frame::generateImage(){
     //loop through each pixel and set the pixel color
     for (int i = 0; i < pixelDimension * pixelDimension; i++){
         frameImage.setPixelColor(QPoint(currentX, currentY), pixels.at(i));
+        //if the currentX is outside the dimensions of the canvas set it to 0
         if (currentX >= pixelDimension - 1){
             currentX = 0;
             currentY += 1;
@@ -72,9 +76,13 @@ void Frame::modifyPixel(QVector<QColor> &pixels, int x, int y, QColor color){
 }
 
 QColor Frame::getPixelColor(int x, int y){
+    //get the xPixel location on the canvas
     int xPixel = ((double)pixelDimension * ((double)x / canvasSize));
+
+    //get the yPixel location on the canvas
     int yPixel = ((double)pixelDimension * ((double)y / canvasSize));
 
+    //get the index of the pixel
     int location = xPixel + (yPixel * pixelDimension);
 
     return pixels[location];
@@ -96,6 +104,7 @@ QVector<QColor> Frame::undoAction(){
         while(history.top() == pixels && history.size() > 1){
             history.pop();
         }
+        //swap the versions
         auto lastVersion = history.top();
         pixels = lastVersion;
         return pixels;
@@ -106,6 +115,7 @@ QVector<QColor> Frame::undoAction(){
 }
 
 void Frame::setPixels(QVector<QColor> newPixels) {
+    //make sure the newPixels is the same size as the pixels
     if (newPixels.size() == pixelDimension * pixelDimension) {
         pixels = newPixels;
         return;
--- frame.h
+++ frame.h
@@ -5,34 +5,110 @@
 #include <QColor>
 #include <stack>
 #include <QImage>
-#include <mutex>
+#include<mutex>
 
+///@brief Header file for the Frame class. represents a frame in the view
+///
+/// Reviewed by: Samir Sarin
 class Frame
 {
 private:
-    //bool firstStroke;
+
+    ///
+    /// @brief pixels holds the colors of all the current pixels
     QVector<QColor> pixels;
+
+    ///
+    /// @brief canvasSize is the size of the canvas
     int canvasSize;
+
+    ///
+    /// @brief pixelSize to be shown on the canvas
     int pixelSize;
+
+    ///
+    /// @brief pixelDimension holds the dimensions of a single pixel
     int pixelDimension;
+
+    ///
+    /// @brief modifyPixel changes the color of the pixel at x and y
+    /// @param pixels
+    /// @param x location in the canvas
+    /// @param y location in the canvas
+    /// @param color that is going to be set
     void modifyPixel(QVector<QColor> &pixels, int x, int y, QColor color);
     std::stack<QVector<QColor>> history;
     QImage frameImage;
 
 public:
+    ///
+    /// @brief Frame constructor
+    /// @param pixelDimension
     Frame(int pixelDimension);
+
+
+    ///
+    /// @brief addNewPixel to the fram
+    /// @param x location on the canvas
+    /// @param y location on the canvas
+    /// @param color
+    /// @return the color of the new pixels
     QVector<QColor> addNewPixel(int x, int y, QColor color);
+
+    ///
+    /// @brief getPixels
+    /// @return returns the pixels
     QVector<QColor> getPixels();
+
+    ///
+    /// @brief addTemporaryPixel
+    /// @param x
+    /// @param y
+    /// @param color
+    /// @return
     QVector<QColor> addTemporaryPixel(int x, int y, QColor color);
+
+    ///
+    /// @brief getPixelColor returns the current color of the given pixel
+    /// @param x location on the canvas
+    /// @param y location on the canvas
+    /// @return the current color
     QColor getPixelColor(int x, int y);
+
+    ///
+    /// @brief undoAction reverts back to the last vector in the history
+    /// @return the last vector of all the pixels
     QVector<QColor> undoAction();
+
+    ///
+    /// @brief addToHistory adds the action that was just made to the history
+    /// @param pixels the current vector of all the pixels
     void addToHistory(QVector<QColor> pixels);
+
+    ///
+    /// @brief clearHistory clears the history of the frame
     void clearHistory();
+
+    ///
+    /// @brief generateImage generates an image and calls set pix
+    /// @return the generated image
     QImage generateImage();
+
+    ///
+    /// @brief getImage
+    /// @return the image of the frame
     QImage getImage();
+
+    ///
+    /// @brief setPixels sets the pixels on the frame
+    /// @param newPixels is a list of the new Pixels to be added
     void setPixels(QVector<QColor> newPixels);
     int ID;
     static int globalID;
+
+    /// @brief operator == copy constructor
+    /// @param other the other frame
+    /// @return if the two frames are e
     bool operator==(const Frame& other) const;
 };
 
